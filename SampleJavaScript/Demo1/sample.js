75cc420ee7b096a969457781ed0d3fdb65ccb681
karunakaran86 <prassan_karthi@yahoo.co.in>
2015-07-15 16:24:47 -0400

    code changes

---
+++ SampleJavaScript/Demo1/sample.js
@@ -0,0 +1,10 @@
+function submit(username,password){
+//alert("function" + username + password)	
+/*document.write("the username entered is :"+username) 
+document.write("the password typed is "+password)*/
+document.getElementById("display").innerHTML=username;
+var x=5;
+var y=7;
+var z=x*y;
+document.getElementById("result").innerHTML=z;
+}
