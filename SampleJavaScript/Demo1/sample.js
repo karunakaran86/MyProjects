a73aeb69a794fd8951ae6f58b0e5129f0578970e
karunakaran86 <prassan_karthi@yahoo.co.in>
2015-07-16 11:49:57 -0400

    added code fo if condition

--- SampleJavaScript/Demo1/sample.js
+++ SampleJavaScript/Demo1/sample.js
@@ -7,4 +7,24 @@ var x=5;
 var y=7;
 var z=x*y;
 document.getElementById("result").innerHTML=z;
+
+
+
+var person={name:"John",age:28 ,weight:201};
+document.getElementById("testing").innerHTML = person.name+"is"+person.age + "years old";
+ }
+
+function wish(){
+	var greeting;
+	var time=new Date().getHours();
+	if (time < 10)
+	{
+		greeting="Good Morning";
+	}else if (time <20){
+		greeting="Good Afternoon"
+	}else
+	{
+		greeting="Good Night"
+	}
+	document.getElementById("wish").innerHTML=greeting;
 }
